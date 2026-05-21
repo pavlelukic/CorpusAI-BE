3cb05e999dc75261628b46bd81de781c1cbf71fe
pavlelukic <pavlelukic2015@gmail.com>
2026-05-21 19:05:24 +0200

    Setup Spring Boot 3.5 project using Spring Initializer with various useful dependencies which will be used in the project, included langchain4j related ones. Populated the application.properties file.

---
+++ src/main/java/com/corpusai/CorpusAiApplication.java
@@ -0,0 +1,13 @@
+package com.corpusai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpusAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpusAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corpusai/CorpusAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corpusai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpusAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
